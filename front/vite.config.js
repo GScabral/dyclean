d62f1caff92f9ac6d242912f1696993fe897a794
GsCabral <cabralgonzalo10@gmail.com>
2026-03-09 14:26:51 -0300

    correciones front

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Cambiado a @vitejs/plugin-react
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()], // Usa el plugin correcto para React
+  plugins: [react()],
   server: {
-    host: '0.0.0.0', // Asegúrate de tener esto para el despliegue
-    port: 3003,       // El puerto que estés usando
-  },
-});
+    allowedHosts: "all"
+  }
+})
