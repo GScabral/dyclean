7e3a70781c738c6199e59bffb0311df207111b88
GsCabral <cabralgonzalo10@gmail.com>
2026-03-31 10:46:48 -0300

    cambios rutas

--- front/src/redux/action/personaEdifiAction.js
+++ front/src/redux/action/personaEdifiAction.js
@@ -4,7 +4,7 @@ import api from "../api/axiosConfig";
 export const quitarAsignacionPersonaEdificio = (id) => {
     return async (dispatch) => {
         try {
-            const response = await api.put(`/personaEdifi/quitar/${id}`);
+            const response = await api.put(`/persona-edificio/quitar/${id}`);
             // Puedes despachar una acción si quieres actualizar el store
             return response;
         } catch (error) {
@@ -18,7 +18,7 @@ export const quitarAsignacionPersonaEdificio = (id) => {
 export const asignarPersonaEdificio = (data) => {
     return async (dispatch) => {
         try {
-            const response = await api.post("/personaEdifi/asignar", data);
+            const response = await api.post("/persona-edificio/asignar", data);
             // Puedes despachar una acción si quieres actualizar el store
             return response;
         } catch (error) {
